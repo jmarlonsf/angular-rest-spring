2eb9948978725b18ab00387b988a9fe60cbeb20a
resource222 (Joao Marlon Souto Ferraz) <RESOURCE222@fornecedor.gamasaude.com.br>
2018-05-23 18:09:37 -0300

    Removendo pessoa com DELETE

--- angular-rest-spring/algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
+++ angular-rest-spring/algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -1,11 +1,12 @@
 package com.example.algamoney.api.model;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
@@ -13,6 +14,9 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
+	@Size(min = 3, max = 20)
 	private String nome;
 	
 	public Long getCodigo() {
