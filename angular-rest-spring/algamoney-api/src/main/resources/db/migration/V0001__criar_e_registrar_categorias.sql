b0f27292d3c3b28192447aede8654cf20c348ae9
Joao Marlon <marlonsouto@gmail.com>
2018-06-13 23:59:16 -0300

    Atualizacao dos dados do banco

--- angular-rest-spring/algamoney-api/src/main/resources/db/migration/V0001__criar_e_registrar_categorias.sql
+++ angular-rest-spring/algamoney-api/src/main/resources/db/migration/V0001__criar_e_registrar_categorias.sql
@@ -1,10 +1,10 @@
 CREATE TABLE categoria (
 	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-	nome 	VARCHAR(50) NOT NULL
+	nome VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categoria (nome) values ('lazer');
-INSERT INTO categoria (nome) values ('alimentacao');
-INSERT INTO categoria (nome) values ('supermercado');
-INSERT INTO categoria (nome) values ('farmacia');
-INSERT INTO categoria (nome) values ('outros');
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
